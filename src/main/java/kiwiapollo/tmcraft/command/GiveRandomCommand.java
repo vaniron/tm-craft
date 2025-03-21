973afa10509688f1cb5b325a9b3cfea304aa4881
Yorick_06 <unconfigured@null.spigotmc.org>
2025-03-21 15:59:04 +0100

    giveRandom now supports bottle caps and mints

--- src/main/java/kiwiapollo/tmcraft/command/GiveRandomCommand.java
+++ src/main/java/kiwiapollo/tmcraft/command/GiveRandomCommand.java
@@ -1,11 +1,13 @@
 package kiwiapollo.tmcraft.command;
 
+import com.cobblemon.mod.common.Cobblemon;
 import com.mojang.brigadier.Command;
 import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.arguments.StringArgumentType;
 import com.mojang.brigadier.context.CommandContext;
 import com.mojang.brigadier.suggestion.Suggestions;
 import com.mojang.brigadier.suggestion.SuggestionsBuilder;
+import kiwiapollo.tmcraft.TMCraft;
 import kiwiapollo.tmcraft.item.tmmove.TMMoveItems;
 import net.minecraft.command.argument.EntityArgumentType;
 import net.minecraft.item.Item;
@@ -23,6 +25,7 @@ import net.minecraft.util.Formatting;
 import net.minecraft.util.Identifier;
 
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
@@ -55,36 +58,47 @@ public class GiveRandomCommand {
 
     private static int giveRandom(ServerCommandSource source, ServerPlayerEntity player, String typeName) {
         Type type = Type.get(typeName);
-        if(type == null) {
+        if (type == null) {
             source.sendError(Text.literal("Unknown type: " + typeName));
             return 0;
         }
 
-        Item randomItem = type.randomSupplier.get();
+        Item randomItem = type.getRandom();
         player.getInventory().offerOrDrop(new ItemStack(randomItem));
         source.sendMessage(Text.literal("Gave [" + Registries.ITEM.getId(randomItem) + "] to " + player.getGameProfile().getName()).formatted(Formatting.GREEN));
         return Command.SINGLE_SUCCESS;
     }
 
     public enum Type {
-        TM(() -> {
-            TMMoveItems[] items = TMMoveItems.values();
-            TMMoveItems randomTm = items[(int)Math.round((items.length - 1) * Math.random())];
-            return randomTm.getItem();
-        }),
-        TYPE_GEM(() -> {
-            Optional<RegistryEntryList.Named<Item>> matches = Registries.ITEM.getEntryList(TagKey.of(Registries.ITEM.getKey(), Identifier.of("cobblemon", "type_gems")));
-            if(matches.isPresent()) {
-                List<Item> items = matches.get().stream().map(RegistryEntry::value).toList();
-                return items.get((int)Math.round((items.size() - 1) * Math.random()));
+        TM(() -> Arrays.stream(TMMoveItems.values()).map(TMMoveItems::getItem).toList()),
+        TYPE_GEM(() -> getTag(Identifier.of(Cobblemon.MODID, "type_gems"))),
+        MINT(() -> getTag(Identifier.of(Cobblemon.MODID, "mints"))),
+        BOTTLE_CAP(() -> getTag(Identifier.of(TMCraft.MOD_ID, "stat_bottle_caps")));
+        private final Supplier<List<Item>> listSupplier;
+        Type(Supplier<List<Item>> listSupplier) {
+            this.listSupplier = listSupplier;
+        }
+
+        private static List<Item> getTag(Identifier tagId) {
+            TagKey<Item> tag = TagKey.of(Registries.ITEM.getKey(), tagId);
+            Optional<RegistryEntryList.Named<Item>> matches = Registries.ITEM.getEntryList(tag);
+            if (matches.isEmpty()) {
+                TMCraft.LOGGER.error("Error while getting random items from tag " + tag + " - tag could not be found");
+                return List.of(Items.STONE);
             }
 
-            return Items.STONE;
-        });
+            List<Item> items = matches.get().stream().map(RegistryEntry::value).toList();
+            if(items.isEmpty()) {
+                TMCraft.LOGGER.error("Error while getting random items from tag " + tag + " - tag is empty");
+                return List.of(Items.STONE);
+            }
 
-        private final Supplier<Item> randomSupplier;
-        Type(Supplier<Item> randomSupplier) {
-            this.randomSupplier = randomSupplier;
+            return items;
+        }
+
+        public Item getRandom() {
+            List<Item> items = this.listSupplier.get();
+            return items.get((int)Math.round((items.size() - 1) * Math.random()));
         }
 
         public static Type get(String name) {
